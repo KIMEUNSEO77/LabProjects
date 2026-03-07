53473f6442eb9b0740eb8e5fc5d08036dbbbd9a3
김은서 <@DESKTOP-FF1I6II>
2026-03-07 23:59:02 +0900

    InitInstance 함수 수정(주 윈도우 생성 시 OnCreate 함수를 호출해 프레임워크 객체 초기화

--- LabProject02/LabProject02.cpp
+++ LabProject02/LabProject02.cpp
@@ -129,13 +129,19 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+   RECT rc = { 0, 0, FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT };
+   DWORD dwStyle = WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_SYSMENU | WS_BORDER;
+   AdjustWindowRect(&rc, dwStyle, FALSE);
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, dwStyle, CW_USEDEFAULT,
+      CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, hInstance, nullptr);
 
    if (!hWnd)
    {
       return FALSE;
    }
+
+   gGameFramework.OnCreate(hInstance, hWnd); // 게임 프레임워크 초기화
 
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
