1e6994b74e46e4c5b567c5ae1f87bb34ba76c495
김은서 <@DESKTOP-FF1I6II>
2026-03-09 00:16:35 +0900

    CPoint3D, CVertex, CPolygon 클래스 생성

--- LapProject02-1/Mesh.h
+++ LapProject02-1/Mesh.h
@@ -1,5 +1,45 @@
 #pragma once
+
 class CMesh
 {
+};
+
+class CPoint3D
+{
+public:
+	CPoint3D() {}
+	CPoint3D(float x, float y, float z) {this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	virtual ~CPoint3D() {}
+
+	float x = 0.0f;
+	float y = 0.0f;
+	float z = 0.0f;
+};
+
+class CVertex
+{
+public:
+	CVertex() {}
+	CVertex(float x, float y, float z) { m_f3Position = CPoint3D(x, y, z); }
+	virtual ~CVertex() {}
+
+	CPoint3D m_f3Position;
+};
+
+class CPolygon
+{
+public:
+	CPolygon() {}
+	CPolygon(int nVertices) {};
+	virtual ~CPolygon() {}
+
+	// 다각형(면)을 구성하는 정점들의 리스트
+	int m_nVertices = 0;
+	CVertex* m_pVertices = nullptr;
+
+	void SetVertex(int nIndex, CVertex vertex);
 };
 
