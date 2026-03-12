57c0e2b046f9af71d928ab22d17f9fbbfa9754de
김은서 <2023180012@tukorea.ac.kr>
2026-03-12 13:09:50 +0900

    cos함수를 이용해 검정<->하양 색상 변화를 표현

--- LabProject05-01/Shaders.hlsl
+++ LabProject05-01/Shaders.hlsl
@@ -61,17 +61,11 @@ float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
     
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f; // (0, 1) : (-0.5, 0.5)
-    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
+    float2 f2NDC = float2(input.x - FRAME_BUFFER_WIDTH * 0.5f, input.y - FRAME_BUFFER_HEIGHT * 0.5f);
+    f2NDC *= 20.0f;    // 좌표를 20배 확대하여 색 변화를 극대화
     
-    float2 f2Horizontal = float2(0.1f, +0.3f); //(Left, Right)
-    float2 f2Vertical = float2(-0.3f, +0.3f); //(Top, Bottom)
-    
-    // x영역 0.1~0.3, y영역 -0.2~0.4인 사각형이 그려짐
-    cColor.b = Rectangle(f2NDC, +0.1f, +0.3f, -0.2f, +0.4f);
-    
-    // x영역 -0.3~-0.1, y영역 -0.1~-0.4인 사각형이 그려짐
-    cColor.b += Rectangle(f2NDC, -0.3f, -0.1f, -0.4f, -0.1f);
+    float fLength = length(f2NDC);  // 중심에서 거리 계산
+    cColor.rgb = cos(fLength); // 원점에서 멀어질수록 색이 변화하도록 함
     
     return (cColor);
 }
