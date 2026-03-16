4d4c2e99e3d3a7cbc5d7ec8e2b6742acc3f7dda2
김은서 <2023180012@tukorea.ac.kr>
2026-03-16 23:44:54 +0900

    일단 복붙....

--- LabProject06/Scene.h
+++ LabProject06/Scene.h
@@ -8,16 +8,30 @@ class CScene
 public:
 	CScene();
 	~CScene();
-	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
-	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM lParam);
-	void CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
-	void CreateGraphicsPipelineState(ID3D12Device* pd3dDevice);
-	void BuildObjects(ID3D12Device* pd3dDevice);
+
+	//씬에서 마우스와 키보드 메시지를 처리한다.
+	bool OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	bool OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM wParam, LPARAM
+		lParam);
+	void BuildObjects(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList
+		* pd3dCommandList);
 	void ReleaseObjects();
-	bool ProcessInput();
+	bool ProcessInput(UCHAR* pKeysBuffer);
 	void AnimateObjects(float fTimeElapsed);
-	void PrepareRender(ID3D12GraphicsCommandList* pd3dCommandList);
 	void Render(ID3D12GraphicsCommandList* pd3dCommandList);
+
+	void ReleaseUploadBuffers();
+
+	//그래픽 루트 시그너쳐를 생성한다.
+	ID3D12RootSignature* CreateGraphicsRootSignature(ID3D12Device* pd3dDevice);
+	ID3D12RootSignature* GetGraphicsRootSignature();
+
+protected:
+	//씬은 셰이더들의 집합이다. 셰이더들은 게임 객체들의 집합이다.
+	CShader** m_ppShaders = NULL;
+	int m_nShaders = 0;
+
 
 	// 렌더링 파이프라인의 "설정 정보"를 GPU에게 전달하는 중요한 객체들
 	// 
