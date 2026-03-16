4b517e911f01b7b574a7d7621cbf79f312d670c0
김은서 <2023180012@tukorea.ac.kr>
2026-03-16 23:21:00 +0900

    stdafx.h수정

--- LabProject06/stdafx.h
+++ LabProject06/stdafx.h
@@ -53,3 +53,9 @@ using Microsoft::WRL::ComPtr; // ComPtr을 사용하기 위한 네임스페이�
 #pragma comment(lib, "winmm.lib")
 
 #define _WITH_SWAPCHAIN_FULLSCREEN_STATE
+
+extern ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice,
+	ID3D12GraphicsCommandList* pd3dCommandList, void* pData, UINT nBytes, D3D12_HEAP_TYPE
+	d3dHeapType = D3D12_HEAP_TYPE_UPLOAD, D3D12_RESOURCE_STATES d3dResourceStates =
+	D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, ID3D12Resource** ppd3dUploadBuffer =
+	NULL);
