6dd69b9d7779ac76a6c7f10ac99dac76d393f6ec
김은서 <2023180012@tukorea.ac.kr>
2026-03-24 00:03:44 +0900

    카메라 변수 관련으로 함수들 다 수정

--- LabProject07/Scene.h
+++ LabProject07/Scene.h
@@ -19,7 +19,7 @@ public:
 
 	bool ProcessInput(UCHAR* pKeysBuffer);
 	void AnimateObjects(float fTimeElapsed);
-	void Render(ID3D12GraphicsCommandList* pd3dCommandList);
+	void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera);
 
 	void ReleaseUploadBuffers();
 
