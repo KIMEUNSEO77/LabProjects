60d7be6a86e40355a3ddfa198b7d0b44d2c62f73
김은서 <2023180012@tukorea.ac.kr>
2026-03-30 23:01:07 +0900

    LabProject08 생성 - 회전하는 직육면체 그리기

---
+++ LabProject08/GameObject.h
@@ -0,0 +1,50 @@
+// GameObject.h
+#pragma once
+
+#include "Mesh.h"
+#include "Camera.h"
+
+class CShader;
+
+class CGameObject
+{
+public:
+	CGameObject();
+	virtual ~CGameObject();
+private:
+	int m_nReferences = 0;
+public:
+	void AddRef() { m_nReferences++; }
+	void Release() { if (--m_nReferences <= 0) delete this; }
+protected:
+	XMFLOAT4X4 m_xmf4x4World;  // 월드 변환 행렬 (이 오브젝트를 어디에 놓을지)
+	CMesh* m_pMesh = NULL;     // 이 오브젝트가 어떤 모양인지
+	CShader* m_pShader = NULL; // 어떻게 그릴지
+public:
+	void ReleaseUploadBuffers();
+	virtual void SetMesh(CMesh* pMesh);
+	virtual void SetShader(CShader* pShader);
+	virtual void Animate(float fTimeElapsed);  // 시간에 따라 움직임 (이동, 회전, 신축 등)
+	virtual void OnPrepareRender();            // 렌더 전 상태 준비
+	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList, CCamera* pCamera); // 실제 그릴지
+
+	void Rotate(XMFLOAT3* pxmf3Axis, float fAngle); // 오브젝트를 회전시키는 함수
+};
+
+class CRotatingObject : public CGameObject
+{
+public:
+	CRotatingObject();
+	virtual ~CRotatingObject();
+private:
+	XMFLOAT3 m_xmf3RotationAxis;
+	float m_fRotationSpeed;
+public:
+	void SetRotationSpeed(float fRotationSpeed) { m_fRotationSpeed = fRotationSpeed; }
+	void SetRotationAxis(XMFLOAT3 xmf3RotationAxis) {
+		m_xmf3RotationAxis =
+			xmf3RotationAxis;
+	}
+	virtual void Animate(float fTimeElapsed);
+};
+
---
+++ LabProject08/Shaders.hlsl
@@ -0,0 +1,51 @@
+// Shader.hlsl
+
+// HLSL(High-Level Shading Language)로, DirectX에서 GPU를 작성하는 언어
+// 
+
+// CPU에서 만든 값 → GPU 셰이더로 전달하기 위해 상수 버퍼(Constant Buffer)를 선언
+// CPU에서 계산한 행렬을 셰이더가 받아서 사용, 즉 실제 3D 렌더링이 시작된 것
+
+// 게임 객체의 정보를 위한 상수 버퍼를 선언
+cbuffer cbGameObjectInfo : register(b0)
+{
+    matrix gmtxWorld : packoffset(c0);
+};
+// 카메라의 정보를 위한 상수 버퍼를 선언
+cbuffer cbCameraInfo : register(b1)
+{
+    matrix gmtxView : packoffset(c0);
+    matrix gmtxProjection : packoffset(c4);
+};
+
+// 정점 셰이더의 입력을 위한 구조체를 선언
+struct VS_INPUT
+{
+    float3 position : POSITION;
+    float4 color : COLOR;
+};
+
+// 정점 셰이더의 출력(픽셀 셰이더의 입력)을 위한 구조체를 선언
+struct VS_OUTPUT
+{
+    float4 position : SV_POSITION;
+    float4 color : COLOR;
+};
+
+// 정점 셰이더를 정의
+VS_OUTPUT VSDiffused(VS_INPUT input)
+{
+    VS_OUTPUT output;
+    
+// 정점을 변환(월드 변환, 카메라 변환, 투영 변환) - GPU에서 직접 계산을 하기 때문에 매우 빠르게 처리됨
+    output.position = mul(mul(mul(float4(input.position, 1.0f), gmtxWorld), gmtxView), gmtxProjection);
+    output.color = input.color;
+    
+    return (output);
+}
+
+//픽셀 셰이더를 정의한다.
+float4 PSDiffused(VS_OUTPUT input) : SV_TARGET
+{
+    return (input.color);
+}
