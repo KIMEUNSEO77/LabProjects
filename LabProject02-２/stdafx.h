9fa66b7b59c257110b1c5a2e3513f2ed97ccf4d2
김은서 <2023180012@tukorea.ac.kr>
2026-04-06 00:11:01 +0900

    필요한 헤더, 매크로, namespace 추가

--- LabProject02-２/stdafx.h
+++ LabProject02-２/stdafx.h
@@ -22,3 +22,31 @@
 
 // DegreeToRadian : 각도를 라디안으로 변환하는 매크로
 #define DegreeToRadian(x) float((x)*3.141592654f/180.0f)
+
+#include <Mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <DirectXColors.h>
+#include <DirectXCollision.h>
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+#define DIR_FORWARD		0x01 
+#define DIR_BACKWARD	0x02
+#define DIR_LEFT		0x04
+#define DIR_RIGHT		0x08
+#define DIR_UP			0x10
+#define DIR_DOWN		0x20
+
+namespace Matrix4x4
+{
+	inline XMFLOAT4X4 Identity()
+	{
+		XMFLOAT4X4 xmmtx4x4Result;
+		XMStoreFloat4x4(&xmmtx4x4Result, XMMatrixIdentity());
+
+		return(xmmtx4x4Result);
+	}
+}
+
