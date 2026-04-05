b40523e97b5f8e8bb7551e656913d6b837c90966
김은서 <2023180012@tukorea.ac.kr>
2026-04-06 01:06:30 +0900

    타이머 변수 및 윈도우 메시지 처리 함수, 마우스 커서 관리 변수 추가

--- LabProject02-２/GameFramework.h
+++ LabProject02-２/GameFramework.h
@@ -4,6 +4,7 @@
 
 #include "Player.h"
 #include "Scene.h"
+#include "Timer.h"
 
 class CGameFramework
 {
@@ -29,6 +30,15 @@ private:
 	// 게임 객체들을 포함하는 씬(게임 세계) 클래스
 	CScene* m_pScene = nullptr;
 
+	// 프레임 레이트를 관리하기 위한 타이머 클래스 객체
+	CGameTimer m_GameTimer;
+
+	//마지막으로 마우스 버튼을 클릭할 때의 마우스 커서의 위치이다.
+	POINT m_ptOldCursorPos;
+
+	//프레임 레이트를 출력하기 위한 문자열이다. 
+	_TCHAR m_pszFrameRate[50];
+
 public:
 	// 프레임워크를 생성하는 함수(주 윈도우가 생성되면 호출)
 	void OnCreate(HINSTANCE hInstance, HWND hMainWnd);
@@ -49,5 +59,13 @@ public:
 	void ProcessInput();
 	void AnimateObjects();
 	void FrameAdvance();
+
+	//윈도우 메시지(키보드, 마우스)를 처리한다.
+	void OnProcessingMouseMessage(HWND hWnd, UINT nMessageID, WPARAM
+		wParam, LPARAM lParam);
+	void OnProcessingKeyboardMessage(HWND hWnd, UINT nMessageID, WPARAM
+		wParam, LPARAM lParam);
+	LRESULT CALLBACK OnProcessingWindowMessage(HWND hWnd, UINT
+		nMessageID, WPARAM wParam, LPARAM lParam);
 };
 
